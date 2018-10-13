8a497818344302ae4268f0567162b807328dffb9
Selochan Lee <selo@coinone.com>
2018-10-13 18:08:16 +0900

    Update Gunicorn App Binding

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,6 +1,7 @@
 import multiprocessing
 
-bind = "0.0.0.0:8000"
+# bind = "0.0.0.0:8000"
+bind = "unix:/tmp/gunicorn.sock"
 workers = multiprocessing.cpu_count() * 2 + 1
 access_logfile = "-"
 error_logfile = "-"
